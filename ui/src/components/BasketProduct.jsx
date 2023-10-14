0c01158ce97b5e7715de3428ad6731228928530f
ahmetesreff <ahmetesreff@gmail.com>
2023-10-14 21:00:33 +0300

    #16 product add done. product remove to do

--- ui/src/components/BasketProduct.jsx
+++ ui/src/components/BasketProduct.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
 
 function BasketProduct(props) {
+
+  const [storageCart, setStorageCart] = useState([]);
+  props = storageCart;
+
+  function handleClick() {
+
+    let shoppingCartText = localStorage.getItem("shoppingCartJSON");
+    const shoppingCart = JSON.parse(shoppingCartText) ?? [];
+    console.log(shoppingCart);
+    console.log(props.productKey);
+    const newArray = shoppingCart.filter(item => item !== props.productKey);
+    console.log(newArray)
+    localStorage.setItem("shoppingCartJSON", JSON.stringify(newArray));
+    setStorageCart(newArray);
+  }
+
+
+
   return (
     <div className="basketItem">
       <Product
         key={props.key}
         name={props.name}
         price={props.price}
+        isFromBasketPage={true}
       />
-      <button className="basketRemoveButton">Sepetten Çıkar</button>
+      {storageCart}
+      <button onClick={handleClick} className="basketRemoveButton">Sepetten Çıkar</button>
     </div>
   );
 }
--- ui/src/components/BasketProductList.jsx
+++ ui/src/components/BasketProductList.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import products from "../service/productService";
 import BasketProduct from "./BasketProduct";
 
-function createList(products) {
+function mapToProduct(products) {
   return (
-    <BasketProduct key={products.key} name={products.name} price={products.price} />
+    <BasketProduct
+      key={products.id}
+      productKey={products.id}
+      name={products.name}
+      price={products.price}
+    />
   );
 }
 
 function BasketProductList() {
+
+  let shoppingCartText = localStorage.getItem("shoppingCartJSON");
+  const shoppingCart = JSON.parse(shoppingCartText) ?? [];
+
   return <>
-  {products.map(createList)}
+    {products.filter((product) => shoppingCart.includes(product.id)).map(mapToProduct)}
   </>;
 }
 
