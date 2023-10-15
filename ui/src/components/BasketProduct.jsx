288cc074453f374a52f04cc3403096ae5285e86d
Akif <akifkarabulut61@gmail.com>
2023-10-15 15:57:34 +0300

    #16 remove from cart feature added

--- ui/src/components/BasketProduct.jsx
+++ ui/src/components/BasketProduct.jsx
@@ -3,34 +3,14 @@ import Product from "./Product";
 
 function BasketProduct(props) {
 
-  const [storageCart, setStorageCart] = useState([]);
-  props = storageCart;
-
-  function handleClick() {
-
-    let shoppingCartText = localStorage.getItem("shoppingCartJSON");
-    const shoppingCart = JSON.parse(shoppingCartText) ?? [];
-    console.log(shoppingCart);
-    console.log(props.productKey);
-    const newArray = shoppingCart.filter(item => item !== props.productKey);
-    console.log(newArray)
-    localStorage.setItem("shoppingCartJSON", JSON.stringify(newArray));
-    setStorageCart(newArray);
-  }
-
-
-
   return (
-    <div className="basketItem">
       <Product
         key={props.key}
+        productKey={props.productKey}
         name={props.name}
         price={props.price}
         isFromBasketPage={true}
       />
-      {storageCart}
-      <button onClick={handleClick} className="basketRemoveButton">Sepetten Çıkar</button>
-    </div>
   );
 }
 
--- ui/src/components/BasketProductList.jsx
+++ ui/src/components/BasketProductList.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import products from "../service/productService";
 import BasketProduct from "./BasketProduct";
 
-function mapToProduct(products) {
+function BasketProductList() {
+  let shoppingCartText = localStorage.getItem("shoppingCartJSON");
+  const shoppingCart = JSON.parse(shoppingCartText) ?? [];
+  const productsChosen = products.filter((product) => shoppingCart.includes(product.id));
+
+  const [storageCart, setStorageCart] = useState(productsChosen);
+
+  function handleClick(event) {
+    let itemToBeRemoved = event.target.id;
+
+    const newArray = storageCart.filter(item => Number(item.id) !== Number(itemToBeRemoved));
+    console.log(JSON.stringify(newArray));
+    localStorage.setItem("shoppingCartJSON", JSON.stringify(newArray));
+    setStorageCart(newArray);
+    console.log(localStorage.getItem("shoppingCartJSON"));
+  }
+
   return (
-    <BasketProduct
-      key={products.id}
-      productKey={products.id}
-      name={products.name}
-      price={products.price}
-    />
+    <>
+      {storageCart.map((product) => (
+        <div className="basketItem">
+          <BasketProduct
+            key={product.id}
+            productKey={product.id}
+            name={product.name}
+            price={product.price}
+          />
+          <button id={product.id} onClick={handleClick} className="basketRemoveButton">
+            Sepetten Çıkar
+          </button>
+        </div>
+      ))}
+    </>
   );
 }
 
-function BasketProductList() {
-
-  let shoppingCartText = localStorage.getItem("shoppingCartJSON");
-  const shoppingCart = JSON.parse(shoppingCartText) ?? [];
-
-  return <>
-    {products.filter((product) => shoppingCart.includes(product.id)).map(mapToProduct)}
-  </>;
-}
 
 export default BasketProductList;
