df69dffa454bd978601623cc8d66e8c0f168101c
Akif <akifkarabulut61@gmail.com>
2023-11-06 21:19:47 +0300

    #3 db connection

--- api/our-project.js
+++ api/our-project.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mysql = require('mysql2')
 const app = express()
 const port = 4000
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "our_project",
+  password: "1234"
+})
+
+// simple query
+connection.query(
+  'SELECT * FROM our_project.product;',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+    console.log(err);
+  }
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
